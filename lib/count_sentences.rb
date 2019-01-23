b047241772a4829edce5eea44fa2ead12a7ce716
Robert McCary <robert.mccary@yahoo.com>
2019-01-23 15:16:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if {|word| word.length < 2}.length 
-    
+    self.split(/\.|\?|\!/).delete_if {|word| word.length < 2}.length
+
   end
 end
